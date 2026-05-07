fe332c68914a7df7bcfcb629414f9702a32ace7a
sean <135666428+seantiz@users.noreply.github.com>
2026-05-07 23:39:07 +0100

    refactor(action.rs): Safer thread variants
    Per clippy, load thread views in a box to account for the size different
    between thread options.
    
    Also remove the about screen task.

--- src/action.rs
+++ src/action.rs
@@ -39,7 +39,7 @@ pub enum Action {
     },
 
     // Thread
-    ThreadLoaded(Option<ThreadViewModel>),
+    ThreadLoaded(Box<Option<ThreadViewModel>>),
 
     // Composer
     OpenComposer {
@@ -79,8 +79,6 @@ pub enum Action {
         cursor: Option<String>,
     },
 
-    // About
-    ShowAbout,
 
     // Errors
     Error(String),
